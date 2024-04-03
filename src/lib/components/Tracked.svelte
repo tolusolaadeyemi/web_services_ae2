1830165fea84125025a4545d136ee9b2fd6bb92a
xaaliax <nc3236pgt@students.nulondon.ac.uk>
2024-04-03 22:28:53 +0100

    the component designs

--- src/lib/components/Tracked.svelte
+++ src/lib/components/Tracked.svelte
@@ -3,7 +3,20 @@
   </script>
 
 <section>
-    <div class="trackd_homes">
+    <div class="tracked_homes">
       <h1>{title}</h1>
     </div>
 </section>
+
+<style>
+.tracked_homes {
+  margin-top: 2rem;
+  font-size: var(--font-size-3);
+  border: 1px solid var(--stone-3);
+  padding: var(--size-3);
+  border-radius: var(--size-2);
+  background-color: white;
+  box-shadow: var(--shadow-2);
+  max-inline-size: 100%;
+}
+</style>
